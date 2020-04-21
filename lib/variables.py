4b3d1ac3e5e3fc488e0e58bc4d4a3699e6b8ce4e
Zhaoyi Shen <pkuszy@gmail.com>
2020-04-20 19:45:27 -0700

    add variable dictionary

---
+++ lib/variables.py
@@ -0,0 +1,5 @@
+var_name_dict_AM4 = {'LWP':'lwp','IWP':'iwp','aer_ex_c_vs':'aod','aer_ab_c_vs':'aod_ab'}
+
+var_name_dict_CAM = {'SWCF':'swcre','LWCF':'lwcre','FSNT':'swnet_toa','FLNT':'lwnet_toa','FSNTC':'swnet_toa_clr','FLNTC':'lwnet_toa_clr','FSNS':'swnet_sfc','FLNS':'lwnet_sfc',
+                 'PRECC':'prec_conv','PRECL':'prec_ls','TS':'t_surf','TREFHT':'t_ref','AEROD_v':'aod',
+                 'CLDLOW':'low_cld_amt','TGCLDLWP':'lwp','TGCLDIWP':'iwp','TGCLDCWP':'cwp','AREL':'reff','AWNC':'cdnc','CCN3':'ccn3'}
