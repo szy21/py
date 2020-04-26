24f602a3fa0ab381106d991793ca846bb7208829
Zhaoyi Shen <pkuszy@gmail.com>
2020-04-25 20:06:07 -0700

    add more variables to the dictionary

--- lib/variables.py
+++ lib/variables.py
@@ -1,5 +1,6 @@
-var_name_dict_AM4 = {'LWP':'lwp','IWP':'iwp','aer_ex_c_vs':'aod','aer_ab_c_vs':'aod_ab'}
+var_name_dict_AM4 = {'LWP':'lwp','IWP':'iwp','aer_ex_c_vs':'aod','aer_ab_c_vs':'aod_ab','low_cld_amt':'low_cld','mid_cld_amt':'mid_cld','high_cld_amt':'high_cld','tot_cld_amt':'tot_cld'}
 
 var_name_dict_CAM = {'SWCF':'swcre','LWCF':'lwcre','FSNT':'swnet_toa','FLNT':'lwnet_toa','FSNTC':'swnet_toa_clr','FLNTC':'lwnet_toa_clr','FSNS':'swnet_sfc','FLNS':'lwnet_sfc',
                  'PRECC':'prec_conv','PRECL':'prec_ls','TS':'t_surf','TREFHT':'t_ref','AEROD_v':'aod',
-                 'CLDLOW':'low_cld_amt','TGCLDLWP':'lwp','TGCLDIWP':'iwp','TGCLDCWP':'cwp','AREL':'reff','AWNC':'cdnc','CCN3':'ccn3'}
+                 'CLDLOW':'low_cld','CLDMED':'mid_cld','CLDHGH':'high_cld','CLDTOT':'tot_cld',
+                 'TGCLDLWP':'lwp','TGCLDIWP':'iwp','TGCLDCWP':'cwp','AREL':'reff','AWNC':'cdnc','CCN3':'ccn3'}
